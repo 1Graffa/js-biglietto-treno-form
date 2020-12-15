a75949a9885f8a3f22a4c645bb6132dc00d0c1e8
Giuseppe <giusepperaffa1988@gmail.com>
2020-12-15 15:40:27 +0100

    init

---
+++ js/script.js
@@ -0,0 +1,21 @@
+// variabili
+var nomeInput = document.getElementById('nome').value;
+var distanza = document.getElementById('km').value;
+var age = document.getElementById('eta').value;
+var prodotto = km * 0.21 ;
+var costoBiglietto = 0.21 * distanza;
+
+
+
+//3)
+//calcolo scontistica in base all'età
+if (age < 18){
+  costoBiglietto = (prodotto - (prodotto * 20 / 100));
+}
+else if (age > 65){
+  costoBiglietto = (prodotto - (prodotto * 40 / 100));
+}
+else {
+  costoBiglietto = prodotto;
+}
+document.getElementById('costoBiglietto').innerHTML += Math.round(costoBiglietto) + "&#36;" ;
